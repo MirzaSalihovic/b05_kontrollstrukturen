0f1a1d6a61b7b922e2eded25c35052ae58a86141
Mirza Salihovic <salihovic.mirza@web.de>
2025-10-09 11:07:17 +0200

    Create file Bewertung2.java

---
+++ e_else/Bewertung2.java
@@ -0,0 +1,27 @@
+// b05_kontrollstruktur
+// e_else
+
+public class Bewertung2
+{
+   public static void main(String[] args)
+   {
+      int punkte;
+      punkte = 49;
+
+      // Eine alternative ("Verzweigung") wird mit dem
+      // Schluesselwort if und einer Bedingung eingeleitet.
+      // Wenn die Bedingung erfuellt ist, wird der
+      // nachfolgende Rumpf ausgefuehrt ("Dann-Zweig").
+      // Sonst wird der Block ausgefuehrt, der dem
+      // Schluesselwort else folgt ("Sonst-Zweig")
+
+      if(punkte >= 50)
+      {
+         System.out.println("Bestanden!");
+      }
+      else
+      {
+         System.out.println("Nicht bestanden");
+      }      
+   }
+}
