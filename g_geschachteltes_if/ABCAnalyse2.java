985184d7eca62653c6285ae7831e27cabc45243d
Mirza Salihovic <salihovic.mirza@web.de>
2025-10-10 09:17:52 +0200

    Create file ABCAnalyse3.java

---
+++ g_geschachteltes_if/ABCAnalyse2.java
@@ -0,0 +1,36 @@
+// b05_kontrollstruktur
+// g_geschachteltes_if
+
+// Schlechter Code
+
+public class ABCAnalyse2
+{
+   public static void main(String[] args)
+   {
+      char prioritaet = 'A';
+
+      if(prioritaet == 'A')
+      {
+         System.out.println("Hoch");
+      }
+      else
+      {
+      }
+         if(prioritaet == 'B')
+         {
+            System.out.println("Mittel");
+         }
+         else
+         {
+            if(prioritaet == 'C')
+            {
+               System.out.println("Niedrig");
+            }
+            else
+            {
+               System.out.println("Unzulässiger Wert");
+            }
+         }
+      
+   }
+}
