c092e2e4df7615098b221a509f136f15bf0299e3
Mirza Salihovic <salihovic.mirza@web.de>
2025-10-14 09:45:57 +0200

    Create file Endlos.java

---
+++ j_endlosSchleife/Endlos.java
@@ -0,0 +1,22 @@
+// b05_kontrollstruktur
+// j_endlosSchleife
+
+public class Endlos
+{
+   public static void main(String[] args)
+   {
+      short i = 1;
+      
+      while(true)
+      {
+         System.out.println(i);
+         
+         i++;                  // Inkrementieren
+         
+         if(i == 10)
+         {
+            break;             // Abbruch der Endlosschleife
+         }
+      }
+   }
+}
